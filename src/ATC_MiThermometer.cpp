06e4caa1cbb6ba9dd31e9700699c0a6ab6e0fc0f
Matthias Prinke <83612361+matthias-bs@users.noreply.github.com>
2022-05-27 12:27:09 +0200

    Changed to a class/into a library

--- src/ATC_MiThermometer.cpp
+++ src/ATC_MiThermometer.cpp
@@ -1,47 +1,59 @@
-/*
-   ATC_MiThermometer.cpp
-   by matthias-bs
-
-   https://github.com/matthias-bs/ESP32_ATC_MiThermometer_Client
-   
-   Based on https://bitbucket.org/dstacer/workspace/snippets/jBoazB/esp32-arduino-ide-ble-scanner-for-xiaomi by David Stacer
-   which in turn is
-   Based on Neil Kolban example for Arduino IDE: https://github.com/nkolban/ESP32_BLE_Arduino/tree/master/examples/BLE_scan
-   Ported to Arduino ESP32 by Evandro Copercini
- 
-   Dumps the ServiceData for 0x1a18 BLE advertisement.
-   For the wonderful firmware located https://github.com/atc1441/ATC_MiThermometer
-   uses BLE library https://github.com/nkolban/ESP32_BLE_Arduino
-
-   Decoding the data was done with help from https://github.com/JsBergbau/MiTemperature2/blob/master/LYWSD03MMC.py
-
-   N.B.:
-   Meanwhile the BLE library by Neil Kolban (https://github.com/nkolban/ESP32_BLE_Arduino) is now part of
-   the Arduino core for ESP32 (https://github.com/espressif/arduino-esp32).
-   
-*/
-
+///////////////////////////////////////////////////////////////////////////////////////////////////
+// ATC_MiThermometer.cpp
+//
+// Bresser 5-in-1/6-in-1 868 MHz Weather Sensor Radio Receiver 
+// based on CC1101 or SX1276/RFM95W and ESP32/ESP8266
+//
+// https://github.com/matthias-bs/ESP32_ATC_MiThermometer_Library
+//
+// Based on:
+// ---------
+// ESP32 BLE for Arduino (https://github.com/espressif/arduino-esp32/tree/master/libraries/BLE)
+// LYWSD03MMC.py by JsBergbau (https://github.com/JsBergbau/MiTemperature2)
+//
+// created: 05/2022
+//
+//
+// MIT License
+//
+// Copyright (c) 2022 Matthias Prinke
+// 
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+// 
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+// 
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+//
+// History:
+//
+// 20220521 Created
+// 20220527 Changed to a class/into a library
+//
+// ToDo: 
+// - Pass list of known sensor's addresses to object
+//
+///////////////////////////////////////////////////////////////////////////////////////////////////
 
 #include "ATC_MiThermometer.h"
-#include <string>
 
-extern MiThData_t miThData[];
-extern BLEScan* pBLEScan;
-extern std::string knownBLEAddresses[];
+//extern std::string knownBLEAddresses[];
 
 // Callback for advertised device found during scan
 class MyAdvertisedDeviceCallbacks: public BLEAdvertisedDeviceCallbacks {
     void onResult(BLEAdvertisedDevice advertisedDevice)
-    {
-//      Serial.printf("Advertised Device: %s \n", advertisedDevice.toString().c_str());
-//      Serial.printf("getServiceDataUUID() %s\n",  advertisedDevice.getServiceDataUUID());
-//      if (advertisedDevice.haveServiceUUID()) {
-//          Serial.println("has service UUID");
-//      }
-//      if ( advertisedDevice.haveName()) {
-//        Serial.println("has Name");
-//      }
-      
+    {      
         if (advertisedDevice.haveServiceData()) {
             char buff[20];
             int datalen;
@@ -78,42 +90,6 @@ class MyAdvertisedDeviceCallbacks: public BLEAdvertisedDeviceCallbacks {
                     }
                     Serial.printf("Device Address: %s\n", advertisedDevice.getAddress().toString().c_str());
                #endif
-               for (int i = 0; knownBLEAddresses[i] != ""; i++) {
-                   #ifdef DEBUG
-                       Serial.printf("Comp.  Address: %s\n", BLEAddress(knownBLEAddresses[i]).toString().c_str());
-                   #endif
-                   if (advertisedDevice.getAddress() == BLEAddress(knownBLEAddresses[i])) {
-                       #ifdef DEBUG
-                           Serial.println("(  known)");
-                       #endif
-                       miThData[i].valid = true;
-                    
-                       // Temperature
-                       int temp_msb = advertisedDevice.getServiceData().c_str()[7];
-                       int temp_lsb = advertisedDevice.getServiceData().c_str()[6];
-                       miThData[i].temperature = (temp_msb << 8) | temp_lsb;
-
-                       // Humidity
-                       int hum_msb = advertisedDevice.getServiceData().c_str()[9];
-                       int hum_lsb = advertisedDevice.getServiceData().c_str()[8];
-                       miThData[i].humidity = (hum_msb << 8) | hum_lsb;
-
-                       // Battery voltage
-                       int volt_msb = advertisedDevice.getServiceData().c_str()[11];
-                       int volt_lsb = advertisedDevice.getServiceData().c_str()[10];
-                       miThData[i].batt_voltage = (volt_msb << 8) | volt_lsb;
-
-                       // Battery state [%]
-                       miThData[i].batt_level = advertisedDevice.getServiceData().c_str()[12];         
-                    
-                       // Received Signal Strength Indicator [dBm]
-                       miThData[i].rssi = advertisedDevice.getRSSI();
-                   } else {
-                      #ifdef DEBUG
-                          Serial.println("(unknown)");
-                      #endif
-                   }
-               } // for (int i = 0; knownBLEAddresses[i] != ""; i++)
            } else {
                #ifdef DEBUG
                    Serial.println(" ServiceDataUUID doesn't match");
@@ -125,20 +101,65 @@ class MyAdvertisedDeviceCallbacks: public BLEAdvertisedDeviceCallbacks {
 
 
 // Set up BLE scanning
-void ATC_MiThermometer_Setup (void)
+void ATC_MiThermometer::begin(void)
 {
     BLEDevice::init("");
-    pBLEScan = BLEDevice::getScan(); //create new scan
-    pBLEScan->setAdvertisedDeviceCallbacks(new MyAdvertisedDeviceCallbacks());
-    pBLEScan->setActiveScan(false); //active scan uses more power, but get results faster
-    pBLEScan->setInterval(100);
-    pBLEScan->setWindow(99);  // less or equal setInterval value
+    _pBLEScan = BLEDevice::getScan(); //create new scan
+    _pBLEScan->setAdvertisedDeviceCallbacks(new MyAdvertisedDeviceCallbacks());
+    _pBLEScan->setActiveScan(false); //active scan uses more power, but get results faster
+    _pBLEScan->setInterval(100);
+    _pBLEScan->setWindow(99);  // less or equal setInterval value
 }
 
+unsigned ATC_MiThermometer::getData(uint32_t duration) {
+    BLEScanResults foundDevices = _pBLEScan->start(duration, false /* is_continue */);
+
+    // Known bug in Arduino BLE library - fix available:
+    // "ESP32 BLE scan, example works but devices found is always 0"
+    // https://forum.arduino.cc/t/esp32-ble-scan-example-works-but-devices-found-is-always-0/876703    
+    for (unsigned i=0; i< foundDevices.getCount(); i++) {
+        for (unsigned n = 0; n < NO_OF_SENSORS; n++) {
+            
+            #ifdef DEBUG
+                Serial.printf("Device Address: %s\n", foundDevices.getDevice(i).getAddress().toString().c_str());
+                Serial.printf("Comp.  Address: %s\n", BLEAddress(knownBLEAddresses[n]).toString().c_str());
+            #endif
+            if (foundDevices.getDevice(i).getAddress() == BLEAddress(knownBLEAddresses[n])) {
+
+                data[n].valid = true;
+            
+                // Temperature
+                int temp_msb = foundDevices.getDevice(i).getServiceData().c_str()[7];
+                int temp_lsb = foundDevices.getDevice(i).getServiceData().c_str()[6];
+                data[n].temperature = (temp_msb << 8) | temp_lsb;
+
+                // Humidity
+                int hum_msb = foundDevices.getDevice(i).getServiceData().c_str()[9];
+                int hum_lsb = foundDevices.getDevice(i).getServiceData().c_str()[8];
+                data[n].humidity = (hum_msb << 8) | hum_lsb;
+
+                // Battery voltage
+                int volt_msb = foundDevices.getDevice(i).getServiceData().c_str()[11];
+                int volt_lsb = foundDevices.getDevice(i).getServiceData().c_str()[10];
+                data[n].batt_voltage = (volt_msb << 8) | volt_lsb;
+
+                // Battery state [%]
+                data[n].batt_level = foundDevices.getDevice(i).getServiceData().c_str()[12];         
+            
+                // Received Signal Strength Indicator [dBm]
+                data[n].rssi = foundDevices.getDevice(i).getRSSI();
+            }
+        }
+    }
+    return foundDevices.getCount();
+}
+
+        
 // Set all array members invalid
-void ATC_MiThermometer_Clear(int no)
+void ATC_MiThermometer::resetData(void)
 {
-    for (int i=0; i < no; i++) {
-        miThData[i].valid = false;
+    for (int i=0; i < NO_OF_SENSORS; i++) {
+        data[i].valid = false;
     }
 }
+
--- src/ATC_MiThermometer.h
+++ src/ATC_MiThermometer.h
@@ -1,26 +1,54 @@
-/*
-   ATC_MiThermometer.h
-   by matthias-bs
+///////////////////////////////////////////////////////////////////////////////////////////////////
+// ATC_MiThermometer.h
+//
+// Bresser 5-in-1/6-in-1 868 MHz Weather Sensor Radio Receiver 
+// based on CC1101 or SX1276/RFM95W and ESP32/ESP8266
+//
+// https://github.com/matthias-bs/ESP32_ATC_MiThermometer_Library
+//
+// Based on:
+// ---------
+// ESP32 BLE for Arduino (https://github.com/espressif/arduino-esp32/tree/master/libraries/BLE)
+// LYWSD03MMC.py by JsBergbau (https://github.com/JsBergbau/MiTemperature2)
+//
+// created: 05/2022
+//
+//
+// MIT License
+//
+// Copyright (c) 2022 Matthias Prinke
+// 
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+// 
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+// 
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+//
+// History:
+//
+// 20220521 Created
+// 20220527 Changed to a class/into a library
+//
+// ToDo: 
+// - Pass list of known sensor's addresses to object
+//
+///////////////////////////////////////////////////////////////////////////////////////////////////
 
-   https://github.com/matthias-bs/ESP32_ATC_MiThermometer_Client
-   
-   Based on https://bitbucket.org/dstacer/workspace/snippets/jBoazB/esp32-arduino-ide-ble-scanner-for-xiaomi by David Stacer
-   which in turn is
-   Based on Neil Kolban example for Arduino IDE: https://github.com/nkolban/ESP32_BLE_Arduino/tree/master/examples/BLE_scan
-   Ported to Arduino ESP32 by Evandro Copercini
- 
-   Dumps the ServiceData for 0x1a18 BLE advertisement.
-   For the wonderful firmware located https://github.com/atc1441/ATC_MiThermometer
-   uses BLE library https://github.com/nkolban/ESP32_BLE_Arduino
 
-   Decoding the data was done with help from https://github.com/JsBergbau/MiTemperature2/blob/master/LYWSD03MMC.py
-
-   N.B.:
-   Meanwhile the BLE library by Neil Kolban (https://github.com/nkolban/ESP32_BLE_Arduino) is now part of
-   the Arduino core for ESP32 (https://github.com/espressif/arduino-esp32).
-   
-*/
-
+#ifndef ATC_MiThermometer_h
+#define ATC_MiThermometer_h
 
 //#define DEBUG
 #include <Arduino.h>
@@ -28,19 +56,74 @@
 #include <BLEUtils.h>
 #include <BLEScan.h>
 #include <BLEAdvertisedDevice.h>
+//#include <string>
 
+// No. of known sensors
+#define NO_OF_SENSORS 2
+
+// BLE adresses of known sensors 
+const std::string knownBLEAddresses[] = {"a4:c1:38:b8:1f:7f", "a4:c1:38:bf:e1:bc"};
 
 // MiThermometer data struct / type
 struct MiThData_S {
-        bool        valid;          // data valid
-        int16_t     temperature;    // temperature x 100°C
-        uint16_t    humidity;       // humidity x 100%
-        uint16_t    batt_voltage;   // battery voltage [mv]
-        uint8_t     batt_level;     // battery level   [%]
-        int16_t     rssi;           // RSSI [dBm]
+        bool        valid;          // !> data valid
+        int16_t     temperature;    // !> temperature x 100°C
+        uint16_t    humidity;       // !> humidity x 100%
+        uint16_t    batt_voltage;   // !> battery voltage [mv]
+        uint8_t     batt_level;     // !> battery level   [%]
+        int16_t     rssi;           // !> RSSI [dBm]
 };
 
 typedef struct MiThData_S MiThData_t;
 
-void ATC_MiThermometer_Setup(void);
-void ATC_MiThermometer_Clear(int no);
+
+/*!
+  \class ATC_MiThermometer
+
+  \brief BLE ATC_MiThermometer thermometer/hygrometer sensor client
+*/
+class ATC_MiThermometer {
+    public:
+        /*!
+        \brief Constructor.
+        */
+        ATC_MiThermometer(/*const std::string known_sensors[]*/) {
+            //_known_sensors = known_sensors;
+        };
+
+        /*!
+        \brief Initialization.
+        */
+        void begin(void);
+        
+        /*!
+        \brief Delete results from BLEScan buffer to release memory.
+        */        
+        void clearScanResults(void) {
+            _pBLEScan->clearResults();
+        };
+        
+        /*!
+        \brief Get data from sensors by running a BLE scan.
+        
+        \param duration Scan duration in seconds
+        */                
+        unsigned getData(uint32_t duration);
+        
+        /*!
+        \brief Set sensor data invalid.
+        
+        \param duration Scan duration in seconds
+        */                        
+        void resetData(void);
+        
+        /*!
+        \brief Sensor data.
+        */
+        MiThData_t data[NO_OF_SENSORS];
+
+protected:
+    //const std::string _known_sensors = known_sensors;
+    BLEScan*    _pBLEScan;
+};
+#endif
